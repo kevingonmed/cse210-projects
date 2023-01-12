51f1f0b8c9cdbf9a07ab4b91473277a9e7d18c56
kevingonmed <kevingonmed@gmail.com>
2023-01-12 16:39:40 -0600

    Week 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string grades = Console.ReadLine();
+        int grade = int.Parse(grades);
+        string letter ="";
+        if (grade>=90)
+        {
+            letter="A";
+        }
+        else if (grade>80)
+        {
+            letter="B";
+        }
+        else if (grade>=70)
+        {
+            
+            letter="C";
+        }
+        else if (grade>=60)
+        {
+            letter="D";
+        }
+        else
+        {
+            letter="F";
+        }
+        Console.WriteLine($"Your grade is {letter}");
+
     }
 }
